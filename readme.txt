a4b7e43f46c32bab26104707ddcbc35632af74e8
wolframalpha <wolframalpha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-15 15:14:27 +0000

    add
    git-svn-id: https://plugins.svn.wordpress.org/wolframalpha-widget/trunk@229604 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Wolfram|Alpha Widget Plugin ===
+Contributors: wolframalpha
+Tags: 
+Requires at least: 2.2
+Tested up to: 2.9.2
+
+Embed live, interactive computational knowledge into your WordPress site with the Wolfram|Alpha Widget plugin.
+
+== Description ==
+
+With the Wolfram|Alpha Widget plugin you can embed live, interactive computational knowledge directly into your WordPress site.
+
+[Learn more](http://products.wolframalpha.com/api/widgets) or [build a Wolfram|Alpha Widget](http://developer.wolframalpha.com/widgetbuilder)
+
+
+== Installation ==
+
+1. Download the Wolfram|Alpha plugin zip file
+1. Go to the Word Press dashboard and choose the 'Add new' option under 'Plugins'.
+1. Click 'Browse' and locate the wolframalpha_wordpress_plugin.zip file downloaded previously.
+1. Click 'Activate Plugin' on the page that follows.
+1. To add the plugin to your sidebar, choose the 'Widgets' option under 'Appearance'. Click and drag the Wolfram|Alpha widget to your sidebar. 
+1. Fill in the id and num fields with values from the publish page or the preview page.
+
+
+== Frequently Asked Questions ==
+
+= What if I have a version of WordPress prior to 2.2? =
+
+You can download a plugin that will allow you to add WordPress widgets to your wordpress blog from [http://automattic.com/code/widgets/][Automattic]
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0 =
+* First Version
---
+++ wolframalpha.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ Plugin Name: Wolfram|Alpha Widget
+ Plugin URI: http://products.wolframalpha.com/api/widgets
+ Description: Embed live, interactive computational knowledge into your WordPress site with the Wolfram|Alpha Widget plugin.
+ Author: Wolfram|Alpha
+ Author URI: http://www.wolframalpha.com
+ Version: 1.0
+*/
+
+function widget_wolframalpha_init() {
+  if ( !function_exists('register_sidebar_widget') ){
+    return;
+  }
+}
+
+
+function widget_wolframalpha() {
+  //get global options
+  if ($args){
+    extract($args);
+  }
+
+  //get widget options
+  $options = get_option('wolframalpha');
+  $id = $options['id'];
+  $num = $options['num'];
+
+  echo $before_widget; //. $before_title . $title . $after_title;
+
+  $script = "<script type=\"text/javascript\" id=\"WolframAlphaScript$num\" src=\"http://developer.wolframalpha.com/widgetbuilder/output/widget.jsp?id=".$id."\"></script>";
+
+  echo $script;
+
+  //sidebar width fixer
+  echo '<script type="text/javascript">jQuery(function(){jQuery("#WolframAlphaWidget$num").width( jQuery("#widget$num table").eq(0).width()+32 );});</script>';
+
+  echo $after_widget;
+}
+
+
+function widget_wolframalpha_control() {
+  $options = get_option('wolframalpha');
+
+  if ( !is_array($options) ){
+    //maybe display a default gallery widget?
+    //$options['id'] = 'DEFAULT_WIDGET_ID';
+  }
+
+  if ( $_POST['widget-control-submit'] ) {
+    $options['id']  = strip_tags(stripslashes($_POST['wolframalphaid']));
+    $options['num'] = strip_tags(stripslashes($_POST['wolframalphanum']));
+    update_option('wolframalpha', $options);
+  }
+
+  $id  = htmlspecialchars($options['id'],  ENT_QUOTES);
+  $num = htmlspecialchars($options['num'], ENT_QUOTES);
+
+  // This will be embedded into the existing settings form.
+  echo '<p style="text-align:right;">';
+  echo '<label for="wolframalphaid" style="width: 40px;">' . __('Id:') . '<input style="width: 140px; id="wolframalphaid" name="wolframalphaid" type="text" value="'.$id.'" /></label><br/>';
+  echo '<label for="wolframalphanum" style="width: 40px;">'. __('Num:'). '<input style="width: 140px; id="wolframalphanum" name="wolframalphanum" type="text" value="'.$num.'" /></label>';
+  echo '</p>';
+
+  echo '<input type="hidden" id="widget-control-submit" name="widget-control-submit" value="1" />';
+}
+
+
+register_sidebar_widget(array('Wolfram Alpha', 'widgets'), 'widget_wolframalpha');
+register_widget_control(array('Wolfram Alpha', 'widgets'), 'widget_wolframalpha_control');
+
+add_action('widgets_init', 'widget_wolframalpha_init');
+
+?>
